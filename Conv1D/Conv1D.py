aa58e4b89f8299d469faffc57ed1d854583f37c7
shunyao <xsy981212@gmail.com>
2021-11-02 15:03:26 -0700

    dataset split

--- Conv1D/Conv1D.py
+++ Conv1D/Conv1D.py
@@ -7,14 +7,32 @@ import scipy.io
 #%% Part1 Data Processing
 dataset1 = scipy.io.loadmat('E:\Stanford_Autumn_2021\CS_230\Project\Dataset\Data_HM\Data_HM\X_Signal\HMNYXZ_signal.mat')
 dataset2 = scipy.io.loadmat('E:\Stanford_Autumn_2021\CS_230\Project\Dataset\Data_HM\Data_HM\X_Signal\HMNYZX_signal.mat')
+labelset1 = scipy.io.loadmat('E:\Stanford_Autumn_2021\CS_230\Project\Dataset\Data_HM\Data_HM\Y_MPS\HMNYXZ_Y.mat')
+labelset2 = scipy.io.loadmat('E:\Stanford_Autumn_2021\CS_230\Project\Dataset\Data_HM\Data_HM\Y_MPS\HMNYZX_Y.mat')
 
-signal_matrix1 = dataset1['signal_matrix']
-signal_matrix2 = dataset2['signal_matrix']
-signal_matrix = np.concatenate((signal_matrix1, signal_matrix2), axis=2)
+signal_matrix = np.concatenate((dataset1['signal_matrix'], dataset2['signal_matrix']), axis=2)
 #print("shape", signal_matrix.shape)
 zeros = np.zeros((65,8,signal_matrix.shape[2]))
 #print("shape", zeros.shape)
 input_matrix = np.concatenate((zeros, signal_matrix), axis=0)
-input_matrix = np.concatenate((input_matrix, zeros), axis=0)
-print("shape", input_matrix.shape)
+input_matrix = np.concatenate((input_matrix, zeros), axis=0).T
+X = input_matrix.reshape(input_matrix.shape[0],-1)
+label = np.concatenate((labelset1['label'], labelset2['label']), axis=0)
+print("Input Shape", X.shape)
+print("Label Shape", label.shape)
 #plt.plot(np.arange(1,201), input_matrix[:,0,0])
+
+#%% Part1 Model Training
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
+
+X = normalize(X, axis=0, norm='max')
+X_train, X_dev, y_train, y_dev = train_test_split(X, label, test_size=0.2)
+print(f"X_train = {X_train.shape}")
+print(X_train)
+print(f"y_train = {y_train.shape}")
+print(y_train)
+print(f"X_dev = {X_dev.shape}")
+print(X_dev)
+print(f"y_dev = {y_dev.shape}")
+print(y_dev)
